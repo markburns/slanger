c80a6d0fadcdea1ac8bf0b1e2a4ea70565e1f34c
Mark Burns <markthedeveloper@gmail.com>
2015-04-28 09:40:27 +0900

    enable healthcheck via e.g. haproxy: add health-check header to use

--- lib/slanger/web_socket_server.rb
+++ lib/slanger/web_socket_server.rb
@@ -22,12 +22,33 @@ module Slanger
 
         EM::WebSocket.start options do |ws|
           # Keep track of handler instance in instance of EM::Connection to ensure a unique handler instance is used per connection.
-          ws.class_eval    { attr_accessor :connection_handler }
-          # Delegate connection management to handler instance.
-          ws.onopen        { |handshake| ws.connection_handler = Slanger::Config.socket_handler.new ws, handshake }
-          ws.onmessage     { |msg| ws.connection_handler.onmessage msg }
-          ws.onclose       { ws.connection_handler.onclose }
+          ws.class_eval    { attr_accessor :connection_handler, :health_check }
 
+          # Delegate connection management to handler instance.
+          ws.onopen        { |handshake|
+            if handshake.headers["health-check"]
+              ws.health_check = true
+            else
+              Slanger.info "Websocket onopen handshake #{handshake.inspect}"
+              handler = Slanger::Config.socket_handler.new(ws, handshake)
+              ws.connection_handler = handler
+            end
+          }
+
+          ws.onmessage     { |msg|
+            Slanger.info "Websocket onmessage msg: #{msg.inspect}"
+            ws.connection_handler.onmessage msg
+          }
+          ws.onclose       {
+            #no-op for healthchecks
+            unless ws.health_check
+              Slanger.info "Websocket onclose socket_id: #{ws}"
+              ws.connection_handler.onclose
+            end
+          }
+
+          # Subscribe the slanger daemon to a Redis channel used for events concerning roster.
+          Slanger::Redis.subscribe 'slanger:connection_notification'
           # Also subscribe the slanger daemon to a Redis channel used for events concerning subscriptions.
           Slanger::Redis.subscribe 'slanger:connection_notification'
         end
