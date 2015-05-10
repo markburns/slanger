fa949e5f30abcf5a2e6d3b997471e5e2a0603fc5
Mark Burns <markthedeveloper@gmail.com>
2015-05-11 07:15:01 +0900

    fix unit test

--- spec/unit/roster_spec.rb
+++ spec/unit/roster_spec.rb
@@ -2,29 +2,15 @@ require 'spec_helper'
 require 'slanger'
 
 describe 'Slanger::Roster' do
-  let(:roster) { Slanger::Roster.new 'presence-channel' }
-  let(:redis) { double "redis" }
+  let(:roster) { Slanger::Presence::Roster.new 'presence-channel' }
 
-  context "success case" do
-    it do
-      expect(Slanger::Redis).to receive(:hgetall).
-        and_return(redis)
-
-      something = double "something", errback: nil
-
-      expect(redis).to receive(:callback).
-        and_yield([1,"2"]).
-        and_return something
+  it "#redis_to_hash" do
+    expect(Slanger::Redis).to receive(:hgetall_sync).and_return [1,"2"]
 
 
-        roster.fetch
-
-        if roster.complete?
-          expect(roster.internal_roster).to eq({1 => 2})
-        end
-    end
-
+    expect(roster.internal_roster).to eq({1 => 2})
   end
+
 end
 
 
